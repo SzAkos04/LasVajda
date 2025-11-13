e30f622ca4dd4554688bc7fd9d3b413b86f8ce74
Ákos Szijgyártó <SzAkos041@gmail.com>
2025-11-13 13:49:03 +0100

    Stacked bar chart implementation

--- public/js/pontok.js
+++ public/js/pontok.js
@@ -14,25 +14,46 @@ onValue(osztalyokRef, (snapshot) => {
     const sortedEntries = Object.entries(data).sort((a, b) => b[1].pont - a[1].pont);
 
     const labels = [];
+    const palack_pontok = [];
     const pontok = [];
 
     sortedEntries.forEach(([key, value]) => {
       labels.push(value.nev);
+      palack_pontok.push(value.palack_pont);
       pontok.push(value.pont);
     });
 
+    console.log(labels);
+    console.log(pontok);
+
     new Chart(ctx, {
-      type: 'bar',
+      type: "bar",
       data: {
         labels: labels,
-        datasets: [{
-          label: 'Pontok',
-          data: pontok,
-          borderWidth: 2
-        }]
+        datasets: [
+          {
+            label: "Pontok",
+            data: pontok,
+            borderWidth: 2
+          },
+          {
+            label: "Palackok",
+            data: palack_pontok,
+            borderWidth: 2
+          },
+          {
+            label: "nigga balls",
+            data: null,
+            borderWidth: 2
+          },
+        ]
       },
       options: {
+        responsive: true, // TODO: !
         scales: {
+          x: {
+            stacked: true
+          },
           y: {
             beginAtZero: true
           }
