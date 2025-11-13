f5277e894b383d31de67b9b15b7b945f13122ea7
Ákos Szijgyártó <SzAkos041@gmail.com>
2025-11-13 12:57:55 +0100

    Chart database integration

--- public/js/pontok.js
+++ public/js/pontok.js
@@ -4,13 +4,40 @@ import { ref, onValue } from "https://www.gstatic.com/firebasejs/12.5.0/firebase
 
 const osztalyokRef = ref(db, "osztalyok");
 
+// Get the canvas element
+const ctx = document.getElementById('myChart');
+
 onValue(osztalyokRef, (snapshot) => {
   const data = snapshot.val();
 
   if (data) {
+    const labels = [];
+    const pontok = [];
+
     Object.entries(data).forEach(([key, value]) => {
-      console.log(`${key}:`, value);
+      labels.push(key);
+      pontok.push(value.pont);
     });
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Pontok',
+          data: pontok,
+          borderWidth: 2
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    })
+
   } else {
     console.log("No data found.");
   }
